8d39c1b55299136369446543ad7b21748bc116e4
ankeris <31132643+ankeris@users.noreply.github.com>
2017-08-31 21:29:28 +0200

    Work on Index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php include 'head.php'; ?>
 
-<main class="middle">
-<article id="logo">
-	<img src="images/logo.png">
-	<a href="chooseLevelPage.php">Start</a>
+<h1 class="middle title">Training Wheel</h1>
+
+<article class="middle">
+	<img class="logo" src="images/logo.png"><br>
 </article>
+
+<section class="middle">
+	<a href="selectLevelPage.php" class="button">Start Training!</a>
+</section>
+
 </main>
