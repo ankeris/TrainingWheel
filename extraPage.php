35180dd864e7c1c2e43f2240ed03419cd4e4aee7
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-09-02 18:56:02 +0200

    Started on the extra/information page

--- extraPage.php
+++ extraPage.php
@@ -1,1 +1,19 @@
 <?php include 'head.php'; ?>
+
+<section class="middle">
+
+    <h1 class="middle title">Information<h1>
+
+    <h2>Introduction<h2>
+    <p class="paragraph">The Training Wheel is a web application made to help people get some extra excersises in a fun and random way</p>
+    <h2>FAQ<h2>
+    <p class="paragraph"></p>
+    <h2>Help<h2>
+    <p class="paragraph"></p>
+    <h2>Contact<h2>
+    <p class="paragraph"></p>
+</section>
+
+
+
+<?php include 'footer.php'; ?>
