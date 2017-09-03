170c286e780c507ef7bd5e92f01771fb2eed7075
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-09-03 13:30:32 +0200

    Applied some css to the extra page

--- extraPage.php
+++ extraPage.php
@@ -1,17 +1,35 @@
 <?php include 'head.php'; ?>
 
-<section class="middle">
+<section class="information" style="
+    max-width: 500px;
+    display: block;
+    text-align: left;
+    margin: auto;
+">
 
-    <h1 class="middle title">Information<h1>
+    <h1 class="middle" style="font-size: 50px;color: #00395E;margin-bottom: 20px;">Information</h1><h1>
 
-    <h2>Introduction<h2>
-    <p class="paragraph">The Training Wheel is a web application made to help people get some extra excersises in a fun and random way</p>
-    <h2>FAQ<h2>
-    <p class="paragraph"></p>
-    <h2>Help<h2>
-    <p class="paragraph"></p>
-    <h2>Contact<h2>
-    <p class="paragraph"></p>
+    </h1><h2>Introduction</h2>
+    <p style="
+    font-size: 20px;
+    font-weight: 100;
+">The Training Wheel is a web application made to help people get some extra excersises in a fun and random way.</p>
+<h2>FAQ</h2><h2>
+    <p style=" font-size: 20px;
+    font-weight: 100;
+">How does it work?</p>
+<p style=" font-size: 20px;
+    font-weight: 100;
+">- You spin the wheel and you get an excersise task.</p>
+    </h2><h2>Help</h2><h2>
+    <p style=" font-size: 20px;
+    font-weight: 100;
+">Have you tried turning it on and off?</p>
+    </h2><h2>Contact</h2><h2>
+    <p style=" font-size: 20px;
+    font-weight: 100;
+">You can contact us at ...@...</p>
+
 </section>
 
 
