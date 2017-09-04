dff8b252bb7a28ecbfdb1c3317235c3bd549c0b6
Juozas Rastenis <aneke1234@gmail.com>
2017-09-04 12:42:36 +0200

    selectLevel href update

--- navigation.php
+++ navigation.php
@@ -1,5 +1,5 @@
 <nav>
-<section class="levelB"><img alt="background" id="level" class="level one" src="images/level1.png"></section>
+<section href="level1.php" class="levelB"><img alt="background" id="level" class="level one" src="images/level1.png"></section>
 <section class="levelB"><img alt="background" class="level two" src="images/level2.png"></section>
 <section class="levelB"><img alt="background" class="level three" src="images/level3.png"></section>
 <section class="levelB"><img alt="background" class="level four" src="images/level4.png"></section>
--- selectLevelPage.php
+++ selectLevelPage.php
@@ -3,39 +3,49 @@
 <main id="selectcontainer">
 <h2 class="title middle">Choose level</h2>
 
-
-<section class="levelcontainer">
-<img src="images/level1.png" alt="level" id="level1">
-	<p class="leveltext">Level one, the kid version, a good start.</p>
-</section>
-
-<figure class="seperator"></figure>
-
-<section class="levelcontainer">
-<img src="images/level2.png" alt="level" id="level2">
-	<p class="leveltext">Level two, the right above beginner.</p>
-</section>
+<a href="level1.php">
+  <section class="levelcontainer">
+    <img src="images/level1.png" alt="level" id="level1">
+  	 <p class="leveltext">Level one, the kid version, a good start.</p>
+  </section>
+</a>
 
 <figure class="seperator"></figure>
 
-<section class="levelcontainer">
-<img src="images/level3.png" alt="level" id="level3">
-	<p class="leveltext">Level three, the middle guy.</p>
-</section>
+<a href="level2.php">
+  <section class="levelcontainer">
+  <img src="images/level2.png" alt="level" id="level2">
+  	<p class="leveltext">Level two, the right above beginner.</p>
+  </section>
+</a>
 
 <figure class="seperator"></figure>
 
-<section class="levelcontainer">
-<img src="images/level4.png" alt="level" id="level4">
-	<p class="leveltext">Level four, if your strong, but not the strongest.</p>
-</section>
+<a href="level3.php">
+  <section class="levelcontainer">
+  <img src="images/level3.png" alt="level" id="level3">
+  	<p class="leveltext">Level three, the middle guy.</p>
+  </section>
+</a>
 
 <figure class="seperator"></figure>
 
-<section class="levelcontainer">
-<img src="images/level5.png" alt="level" id="level5">
-	<p class="leveltext">Level five, hulk version, take it to the extreme!</p>
-</section>
+<a href="level4.php">
+  <section class="levelcontainer">
+  <img src="images/level4.png" alt="level" id="level4">
+  	<p class="leveltext">Level four, if your strong, but not the strongest.</p>
+  </section>
+</a>
+
+<figure class="seperator"></figure>
+
+<a href="level5.php">
+  <section class="levelcontainer">
+  <img src="images/level5.png" alt="level" id="level5">
+  	<p class="leveltext">Level five, hulk version, take it to the extreme!</p>
+  </section>
+</a>
+
 </main>
 <script>
 $(".levelcontainer").hover(
