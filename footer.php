34169e9f7f15bbddc007a7b0a6b832898e7ab1e8
JuozasR <31132643+ankeris@users.noreply.github.com>
2017-09-07 13:49:34 +0200

    added some more script links

--- footer.php
+++ footer.php
@@ -4,4 +4,7 @@
 </body>
 <script src="js/spinwheel.js"></script>
 <script src="js/javascript.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.14/p5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.14/addons/p5.dom.min.js"></script>
+<script src="js/sketch.js"></script>
 </html>
