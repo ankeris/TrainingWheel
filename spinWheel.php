882766c21f002f7c1fc84207cb4e2104502056a7
jonaskkristensen <jonaskkristensen@hotmail.dk>
2017-09-07 12:18:51 +0200

    Levels fixed

--- spinWheel.php
+++ spinWheel.php
@@ -53,15 +53,15 @@
 
   var doSomething;
 
-  if(window.location.href === "http://jonas-k.dk/TrainingWheel/level1.php" || window.location.href.match('localhost') ){
+  if(window.location.href.match('level1') ){
     doSomething = ["10 sit ups", "Plank 25 seconds", "5 dips", "Crab walk for 20 seconds", "10 lounges", "5 inverted rows", "5 push ups", "5 jumping jacks"];
-  } else if(window.location.href === "http://jonas-k.dk/TrainingWheel/level2.php" || window.location.href.match('localhost')) {
+  } else if(window.location.href.match('level2')) {
     doSomething = ["15 sit ups", "Plank 30 seconds", "10 dips", "Crab walk for 25 seconds", "15 lounges", "10 inverted rows", "10 push ups", "10 jumping jacks"];
-  } else if(window.location.href === "http://jonas-k.dk/TrainingWheel/level3.php" || window.location.href.match('localhost')) {
+  } else if(window.location.href.match('level3')) {
     doSomething = ["20 sit ups", "Plank 35 seconds", "15 dips", "Crab walk for 30 seconds", "20 lounges", "15 inverted rows", "15 push ups", "15 jumping jacks"];
-  } else if(window.location.href === "http://jonas-k.dk/TrainingWheel/level4.php" || window.location.href.match('localhost')) {
+  } else if(window.location.href.match('level4')) {
     doSomething = ["25 sit ups", "Plank 40 seconds", "10 Diamond push ups", "Hand stand 20 seconds", "25 lounges", "20 inverted rows", "20 push ups", "20 jumping jacks"];
-  } else if(window.location.href === "http://jonas-k.dk/TrainingWheel/level5.php"|| window.location.href.match('localhost')) {
+  } else if(window.location.href.match('level5')) {
     doSomething = ["30 sit ups", "Plank 45 seconds", "20 Diamond push ups", "Hand stand 40 seconds", "30 lounges", "25 inverted rows", "25 push ups", "25 jumping jacks"];
   } else {
     alert("Something went wrong.");
