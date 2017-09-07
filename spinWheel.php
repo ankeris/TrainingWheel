28087901fc6d4b0a54f12f6732ed4853c255147c
Tim Stroustrup Skov Henriksen <tim.s.h@hotmail.com>
2017-09-07 13:05:49 +0200

    whitebackground

--- spinWheel.php
+++ spinWheel.php
@@ -1,7 +1,8 @@
 <div id="wrapper">
 <input class="buttonWheel" type="button" value="SPIN" id='spin' />
   <canvas id="canvas" width="300" height="400"></canvas>
-</div>
+	<figure id="whitebox"></figure>
+  </div>
 
   <script>
 
