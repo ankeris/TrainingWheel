4875deacd2d27ba585583acbe323f69bc0b47eda
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-09-07 13:40:18 +0200

    change of href's

--- navigation.php
+++ navigation.php
@@ -1,23 +1,23 @@
 <img src="images/plante.png" id="plante" alt="livingroom">
 <main> </main>
 <nav>
-<a href="level1.php">
+<a href="level1">
 	<img alt="background" id="level" class="level one" src="images/level1.png">
 </a>
 
-<a href="level2.php">
+<a href="level2">
 	<img alt="background" class="level two" src="images/level2.png">
 </a>
 
-<a href="level3.php">
+<a href="level3">
 <img alt="background" class="level three" src="images/level3.png">
 </a>
 
-<a href="level4.php">
+<a href="level4">
 <img alt="background" class="level four" src="images/level4.png">
 </a>
 
-<a href="level5.php">
+<a href="level5">
 <img alt="background" class="level five" src="images/level5.png">
 </a>
 
